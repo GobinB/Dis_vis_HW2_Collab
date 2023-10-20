7158528fccb439e1e74414e8725da6e84e017204
GobinB <gobindabastola6@gmail.com>
2023-10-20 14:41:55 -0400

    test02

--- main.py
+++ main.py
@@ -1,42 +1,26 @@
+# Required Libraries
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Step 1: Load the data
-timezone_df = pd.read_csv('subject_timezone_log.csv')
-registry_df = pd.read_csv('subject_registry.csv')
+# Step 1: Load the Data
+subject_registry_df = pd.read_csv("subject_registry.csv")
+subject_timezone_log_df = pd.read_csv("subject_timezone_log.csv")
 
-print("Columns in timezone_df:", timezone_df.columns)
-print("Columns in registry_df:", registry_df.columns)
+# Step 2: Filter the Data
+filtered_subjects = subject_registry_df[(subject_registry_df['ISA'] == 'BP03') & 
+                                        (~subject_registry_df['Actual_Visit'].isin(['ED', 'SF']))]
+eligible_participants = filtered_subjects['SubjectID'].tolist()
+filtered_timezone_data = subject_timezone_log_df[subject_timezone_log_df['subject'].isin(eligible_participants)]
 
-# Rename 'subjectID' to 'subject' for consistency
-registry_df.rename(columns={'SubjectID': 'subject'}, inplace=True)
-print("\nAfter renaming columns in registry_df:", registry_df.columns)
+# Step 3: Process Timezone Data
+filtered_timezone_data['date'] = pd.to_datetime(filtered_timezone_data['timestamp_iso']).dt.date
+timezone_matrix = filtered_timezone_data.pivot_table(index='subject', columns='date', values='timezone_location', aggfunc='first')
 
-# Filter the subjects based on the given conditions
-eligible_subjects = registry_df[
-    (registry_df['ISA'] == 'BP03') & 
-    (~registry_df['Actual_Visit'].isin(['ED', 'SF']))
-]
-print("\nNumber of eligible subjects:", len(eligible_subjects))
-
-# Step 2: Merge the two dataframes
-merged_df = pd.merge(eligible_subjects, timezone_df, on=['Site', 'subject'])
-print("\nNumber of rows in merged_df:", len(merged_df))
-
-# Step 3: Processing the Data
-# Count the distinct timezones for each subject
-timezone_changes = merged_df.groupby('subject')['timezone_location'].nunique()
-print("\nNumber of distinct timezones by subject:", len(timezone_changes))
-
-# Step 4: Visualization
-# For the sake of simplicity, let's represent each subject on the y-axis and their number of timezone changes on the x-axis in a heatmap.
-heatmap_data = timezone_changes.values.reshape(-1, 1)
-
-# Plotting
-plt.figure(figsize=(5, len(timezone_changes)))
-sns.heatmap(heatmap_data, cmap='YlGnBu', yticklabels=timezone_changes.index)
-plt.title("Timezone Changes by Subject")
-plt.xlabel("Number of Timezone Changes")
-plt.ylabel("Subjects")
+# Step 4: Visualize with a Heatmap
+plt.figure(figsize=(20, 10))
+sns.heatmap(timezone_matrix.notnull(), cmap="YlGnBu", cbar=False, yticklabels=True)
+plt.title("Timezone Changes Over the Trial")
+plt.xlabel("Date")
+plt.ylabel("Subject ID")
 plt.show()
