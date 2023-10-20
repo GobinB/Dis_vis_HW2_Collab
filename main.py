f6f0219237d23af63be99522f835958138d5d683
AhmedMurshid <ahmed.abdullahione@gmail.com>
2023-10-20 15:02:57 -0400

    notebook

--- main.py
+++ main.py
@@ -1,42 +1,48 @@
 import pandas as pd
-import seaborn as sns
+import seaborn as sns 
 import matplotlib.pyplot as plt
 
-# Step 1: Load the data
-timezone_df = pd.read_csv('subject_timezone_log.csv')
-registry_df = pd.read_csv('subject_registry.csv')
+def visualize_timezone_changes():
+    # Load the data
+    timezone_df = pd.read_csv('subject_timezone_log.csv')
+    registry_df = pd.read_csv('subject_registry.csv')
 
-print("Columns in timezone_df:", timezone_df.columns)
-print("Columns in registry_df:", registry_df.columns)
+    # Rename 'SubjectID' to 'subject' for consistency
+    registry_df.rename(columns={'SubjectID': 'subject'}, inplace=True)
 
-# Rename 'subjectID' to 'subject' for consistency
-registry_df.rename(columns={'SubjectID': 'subject'}, inplace=True)
-print("\nAfter renaming columns in registry_df:", registry_df.columns)
+    # Filter the subjects based on the given conditions
+    eligible_subjects = registry_df[
+        (registry_df['ISA'] == 'BP03') & 
+        (~registry_df['Actual_Visit'].isin(['ED', 'SF']))
+    ]
 
-# Filter the subjects based on the given conditions
-eligible_subjects = registry_df[
-    (registry_df['ISA'] == 'BP03') & 
-    (~registry_df['Actual_Visit'].isin(['ED', 'SF']))
-]
-print("\nNumber of eligible subjects:", len(eligible_subjects))
+    # Merge the two dataframes
+    merged_df = pd.merge(eligible_subjects, timezone_df, on=['Site', 'subject'])
 
-# Step 2: Merge the two dataframes
-merged_df = pd.merge(eligible_subjects, timezone_df, on=['Site', 'subject'])
-print("\nNumber of rows in merged_df:", len(merged_df))
+    # Count the distinct timezones for each subject
+    timezone_changes = merged_df.groupby('subject')['timezone_location'].nunique()
 
-# Step 3: Processing the Data
-# Count the distinct timezones for each subject
-timezone_changes = merged_df.groupby('subject')['timezone_location'].nunique()
-print("\nNumber of distinct timezones by subject:", len(timezone_changes))
+    # Convert the series to a dataframe and reshape for the heatmap
+    df = timezone_changes.reset_index()
+    df.columns = ['subject', 'num_changes']
+    heatmap_data = df.set_index('subject')['num_changes'].values.reshape(-1, 1)
 
-# Step 4: Visualization
-# For the sake of simplicity, let's represent each subject on the y-axis and their number of timezone changes on the x-axis in a heatmap.
-heatmap_data = timezone_changes.values.reshape(-1, 1)
+    # Plotting
+    # fig, ax = plt.subplots(figsize=(2, 10))
+    # sns.heatmap(heatmap_data, cmap='RdYlGn', linewidths=0.30, annot=True, yticklabels=df['subject'].values)
+    # plt.title("Timezone Changes by Subject")
+    # plt.xlabel("Number of Timezone Changes")
+    # plt.ylabel("Subjects")
+    # plt.show()
 
-# Plotting
-plt.figure(figsize=(5, len(timezone_changes)))
-sns.heatmap(heatmap_data, cmap='YlGnBu', yticklabels=timezone_changes.index)
-plt.title("Timezone Changes by Subject")
-plt.xlabel("Number of Timezone Changes")
-plt.ylabel("Subjects")
-plt.show()
+    fig, ax = plt.subplots(figsize=(16,8))
+    sns.set()
+    sns.heatmap(heatmap_data, cmap='YlGnBu')
+    plt.title("Timezone Changes by Subject")
+    plt.xlabel("Number of Timezone Changes")
+    plt.ylabel("Subjects")
+    plt.xticks(rotation=15)
+    plt.show()
+
+# Call the function to execute
+visualize_timezone_changes()
